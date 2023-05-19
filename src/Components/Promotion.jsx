87bc6d358b9b4bac893c75730596b10ee99983d3
Luka Koridze <lukakoridze13@gmail.com>
2023-05-19 16:49:58 +0400

    Promotion Detection

---
+++ src/Components/Promotion.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { styled } from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChessBishop } from "@fortawesome/free-solid-svg-icons";
+import { faChessKing } from "@fortawesome/free-solid-svg-icons";
+import { faChessKnight } from "@fortawesome/free-solid-svg-icons";
+import { faChessPawn } from "@fortawesome/free-solid-svg-icons";
+import { faChessQueen } from "@fortawesome/free-solid-svg-icons";
+import { faChessRook } from "@fortawesome/free-solid-svg-icons";
+export default function Promotion(props) {
+  let { color } = props;
+  let black = "#1f2a3d";
+  let white = "#e6e5e9";
+  return (
+    <Popup className="promo">
+      <FontAwesomeIcon
+        icon={faChessQueen}
+        style={{
+          color: color === "white" ? white : black,
+          cursor: "pointer",
+        }}
+      />
+      <FontAwesomeIcon
+        icon={faChessRook}
+        style={{
+          color: color === "white" ? white : black,
+          cursor: "pointer",
+        }}
+      />
+      <FontAwesomeIcon
+        icon={faChessKnight}
+        style={{
+          color: color === "white" ? white : black,
+          cursor: "pointer",
+        }}
+      />
+      <FontAwesomeIcon
+        icon={faChessBishop}
+        style={{
+          color: color === "white" ? white : black,
+          cursor: "pointer",
+        }}
+      />
+    </Popup>
+  );
+}
+
+let Popup = styled.div`
+  padding: 30px;
+  border-radius: 20px;
+  background-color: #170101d9;
+  position: absolute;
+  z-index: 100;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
